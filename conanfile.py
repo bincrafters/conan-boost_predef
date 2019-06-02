2ddc8553a7cfe7ef5669822b64244811d3141c9f
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:07 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -2,22 +2,29 @@
 # -*- coding: utf-8 -*-
 
 from conans import python_requires
-import os
+import os.path
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostPredefConan(base.BoostBaseConan):
     name = "boost_predef"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_predef"
-    lib_short_names = ["predef"]
-    header_only_libs = ["predef"]
+    version = "1.70.0"
 
-    def package_additional(self):
-        for lib_short_name in self.lib_short_names:
-            for subdir in ("check","tools"):
-                src_dir = os.path.join(lib_short_name, subdir)
-                self.copy(pattern="*.jam", dst=os.path.join(lib_short_name, "lib", subdir), src=src_dir)
-                self.copy(pattern="*.c*", dst=os.path.join(lib_short_name, "lib", subdir), src=src_dir)
-                self.copy(pattern="*.m*", dst=os.path.join(lib_short_name, "lib", subdir), src=src_dir)
+    def package(self):
+        super(BoostPredefConan, self).package()
+        for subdir in ("check", "tools"):
+            src_dir = os.path.join('predef', subdir)
+            self.copy(
+                pattern="*.jam",
+                dst=os.path.join('predef', "lib", subdir),
+                src=src_dir)
+            self.copy(
+                pattern="*.c*",
+                dst=os.path.join('predef', "lib", subdir),
+                src=src_dir)
+            self.copy(
+                pattern="*.m*",
+                dst=os.path.join('predef', "lib", subdir),
+                src=src_dir)
